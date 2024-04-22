14e3b394a7c6c9883a6d8d5cae00ea907ded2520
patromi <patrom123@gmail.com>
2024-04-22 17:05:59 +0200

    bstree

--- Sprawozdanie nr2/data/Bst.py
+++ Sprawozdanie nr2/data/Bst.py
@@ -1,9 +1,4 @@
 import gc
-
-if not gc.isenabled():
-    gc.enable()
-
-
 class BSTNode:
     def __init__(self, value):
         self.key = value
@@ -36,41 +31,33 @@ class BSTree:
 
     def print_tree(self):
         print("Pre order: ", end='')
-        print(self._get_pre_order_tree(self.root))
+        self.print_pre_order_tree(self.root)
         print("\nIn order: ", end='')
-        print(self._get_in_order_tree(self.root))
+        self._print_in_order_tree(self.root)
         print("\nPost order: ", end='')
-        print(self._get_post_order_three(self.root))
+        self._print_post_order_tree(self.root)
 
-    def _get_pre_order_tree(self, node: BSTNode, arr: list[int] | bool = None, first: bool = True) -> list[int]:
-        if first:
-            arr = [] if arr is None else arr
+    def print_pre_order_tree(self, node):
         if node is not None:
-            arr.append(node.key)
-            self._get_pre_order_tree(node.left, arr, first=False)
-            self._get_pre_order_tree(node.right, arr, first=False)
-        return arr
+            print(node.key, end=' ')
+            self.print_pre_order_tree(node.left)
+            self.print_pre_order_tree(node.right)
 
-    def _get_in_order_tree(self, node: BSTNode, arr: list[int] | bool = None, first: bool = True) -> list[int]:
-        if first:
-            arr = [] if arr is None else arr
+    def _print_in_order_tree(self, node):
         if node is not None:
-            self._get_pre_order_tree(node.left, arr, first=False)
-            arr.append(node.key)
-            self._get_pre_order_tree(node.right, arr, first=False)
-        return arr
+            self._print_in_order_tree(node.left)
+            print(node.key, end=' ')
+            self._print_in_order_tree(node.right)
 
-    def _get_post_order_three(self, node: BSTNode, arr: list[int] | bool = None, first: bool = True) -> list[int]:
-        if first:
-            arr = [] if arr is None else arr
+    def _print_post_order_tree(self, node):
         if node is not None:
-            self._get_pre_order_tree(node.left, arr, first=False)
-            self._get_pre_order_tree(node.right, arr, first=False)
-            arr.append(node.key)
-        return arr
+            self._print_in_order_tree(node.left)
+            self._print_in_order_tree(node.right)
+            print(node.key, end=' ')
 
-    def find_min(self) -> int:
-        node: BSTNode = self.root
+    @staticmethod
+    def find_min(root) -> int:
+        node: BSTNode = root
         temp: int = node.key
         while node.left is not None:
             if node.key < temp:
@@ -119,7 +106,7 @@ class BSTree:
                 return node.right
             elif node.right is None:
                 return node.left
-            node.key = self.find_min()
+            node.key = self.find_min(node.right)
             node.right = self.delete_node(node.right, node.key)
         return node
 
@@ -132,96 +119,13 @@ class BSTree:
         self.delete_all_tree(node.left, first=False)
         self.delete_all_tree(node.right, first=False)
         print(node.key, end=' ')
-        self.root = self.delete_node(self.root, node.key)
-        gc.collect()
+        self.delete_node(self.root, node.key)
 
-    @staticmethod
-    def rotate_right(node):
-        if node is None or node.left is None:
-            return node
-        left_node = node.left
-        node.left = left_node.right
-        left_node.right = node
-        return left_node
-
-    @staticmethod
-    def rotate_left(node):
-
-
-
-    def dsw_balance(self):
-        self.root = self._make_vine(self.root)
-        in_order = self._get_in_order_tree(self.root)[1::2]
-        self.display()
-        print(in_order)
-        for i in in_order:
-            self.root = self.rotate_left(self.find_node(i))
-
-    def _make_vine(self, node):
-        if node is None:
-            return None
-        while node.left is not None:
-            node = self.rotate_right(node)
-        node.right = self._make_vine(node.right)
-        return node
-
-    def display(self):
-        lines, *_ = self._display_aux(self.root)
-        for line in lines:
-            print(line)
-
-    def _display_aux(self, node):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if node.right is None and node.left is None:
-            line = '%s' % node.key
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if node.right is None:
-            lines, n, p, x = self._display_aux(node.left)
-            key_str = '%s' % node.key
-            key_length = len(key_str)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + key_str
-            second_line = x * ' ' + '/' + (n - x - 1 + key_length) * ' '
-            shifted_lines = [line + key_length * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + key_length, p + 2, n + key_length // 2
-
-        # Only right child.
-        if node.left is None:
-            lines, n, p, x = self._display_aux(node.right)
-            key_str = '%s' % node.key
-            key_length = len(key_str)
-            first_line = key_str + x * '_' + (n - x) * ' '
-            second_line = (key_length + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [key_length * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + key_length, p + 2, key_length // 2
-
-        # Two children.
-        left, n, p, x = self._display_aux(node.left)
-        right, m, q, y = self._display_aux(node.right)
-        key_str = '%s' % node.key
-        key_length = len(key_str)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + key_str + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + key_length + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + key_length * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + key_length, max(p, q) + 2, n + key_length // 2
 
 if __name__ == '__main__':
     tree = BSTree([8, 2, 5, 14, 10, 12, 13, 2222, 9])
     tree.print_tree()
     print('\n')
-    tree.display()
-
     # tree.delete_nodes([8, 2, 5, 14, 10, 12, 13, 2222, 9])
-    tree.dsw_balance()
-    tree.display()
+    tree.delete_all_tree(tree.root)
     tree.print_tree()
